748c2c4cf83a464bce1ec81299817117972ca108
Hasna Hena Mow <hasna.hena@welldev.io>
2024-11-14 13:39:16 +0600

    setup websocket to serve data with html every second

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import { createServer } from 'http'
 import { readFile } from 'fs/promises'
+import WebSocket, { WebSocketServer } from 'ws'
 import { join } from 'path'
 import { fileURLToPath } from 'url'
 const PORT = 8080
@@ -17,8 +18,42 @@ const server = createServer(async (req, res) => {
     res.end('Not found')
   }
 })
+
+const wss = new WebSocketServer({ server })
+console.log('WebSocket server created')
+
+wss.on('connection', (ws) => {
+  console.log('Client connected')
+  sendMetrics(ws)
+  ws.on('error', console.error)
+  ws.on('close', () => {
+    console.log('Client disconnected')
+  })
+})
+async function sendMetrics(ws) {
+  try {
+    const metrics = {
+      timestamp: new Date().toISOString(),
+    }
+
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(metrics))
+    }
+  } catch (error) {
+    console.error('Error collecting metrics:', error)
+  }
+}
+
 // Start server
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
   console.log(`Frontend path: ${FRONTEND_PATH}`)
 })
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      sendMetrics(client)
+    }
+  })
+}, 1000)
